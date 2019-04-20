edc7fe8dbb34cc01be7b4342b84e5d419561cc6d
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-04-20 15:44:58 -0700

    working functionality

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ UC-Riverside Bootcamp Homework Assignment #12
 ### Required Packages
 `npm install mysql`
 
-`npm install mysql`
+`npm install chalk`
+
+`npm install inquirer`
 
 Gif of search all (to do this run node bamazonCustomer.js):
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,31 +30,92 @@ connection.connect(function(err) {
     // logs the actual query being run
     //console.log(query.sql);
 
-    function displayAllProducts() {
-        log("Selecting all products...\n");
-        connection.query("SELECT * FROM products", function(err, res) {
-              if (err) throw err;
-              // Log all results of the SELECT statement
-            
-              for (i=0; i<res.length;i++){
-                  log(chalk.red(res[i].product_name));
-                  log(chalk.blue(res[i].department_name));
-                  log(chalk.green(res[i].price));
-                  log(chalk.yellow(res[i].stock_quantity));
-                  log("---------------");
-              }
-          
-              connection.end();
-            });
+function displayAllProducts() {
+    log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+          if (err) throw err;
+          // Log all results of the SELECT statement
+        
+          for (i=0; i<res.length;i++){
+              log(chalk.white("Item ID: ",res[i].item_id));
+              log(chalk.red("Product Name: ", res[i].product_name));
+              log(chalk.blue("Department: ", res[i].department_name));
+              log(chalk.green("Price: ",res[i].price));
+              log(chalk.yellow("# in Stock: ",res[i].stock_quantity));
+              // log(chalk.keyword('orange')("Sales: ",res[i].product_sales));
+              log("---------------");
+          }
+          displayPrompts();
+          // connection.end();
+        });
 
+    function displayPrompts(){
+      let inquirer = require("inquirer");
+
+      inquirer.prompt([
+        {
+        type: "input",
+        name: "id",
+        message: "Please enter an item ID"
+        },
+        {
+        type: "input",
+        name: "quantity",
+        message: "Please enter the quantity you would like to purchase"
+        }
+      ]).then(answers =>{
+        id = answers.id;
+        quantity = answers.quantity;
+        checkQuantity(id, quantity);
+      });
+
+    }
+
+}
+
+function checkQuantity(id, quantity){
   
-//   function readProducts() {
-//     console.log("Selecting all products...\n");
-//     connection.query("SELECT * FROM products where product_name = " + "'" + query + "'", function(err, res) {
-//       if (err) throw err;
-//       // Log all results of the SELECT statement
-//       console.log(res);
-  
-//       connection.end();
-//     });
-  }
+  connection.query(
+    "SELECT * FROM bamazon.products WHERE item_id = '" + id + "';",
+    function (err, res) {
+      if (err) throw err;
+      // if (quantity < 0) {
+      //   quantity = quantity * -1;
+      // }
+      res[0].price = parseFloat(res[0].price);
+      quantity = parseFloat(quantity);
+      res[0].stock_quantity = parseInt(res[0].stock_quantity);
+      res[0].product_sales = parseFloat(res[0].product_sales);
+      if (parseInt(quantity) < parseInt(res[0].stock_quantity)) {
+        updateQuantity = res[0].stock_quantity - quantity;
+        productSales = Number(res[0].product_sales + (quantity * res[0].price));
+       
+        updateProduct(id, updateQuantity, productSales);
+        console.log("You have sucessfully purchased " + quantity + " " + res[0].product_name + " @ " + res[0].price + "/unit.")
+        console.log("Total: $", quantity * res[0].price);
+      } else {
+        console.log("Insufficient quantity!");
+        connection.end();
+      }
+    }
+  )};
+
+function updateProduct(id, quantity, productSales) {
+    connection.query(
+        "UPDATE products SET ?, ? WHERE ?",
+        [
+            {
+                stock_quantity: quantity
+            },{
+                product_sales: productSales
+            },{
+                item_id: id
+            }
+        ],
+        function(err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " products updated!\n");
+            connection.end();
+        }
+    );
+ };
